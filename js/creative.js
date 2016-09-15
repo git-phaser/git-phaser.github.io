5cbdd50b2841e8f0e1d86eefb35392f55b529d0e
cgewecke <christophergewecke@gmail.com>
2016-09-14 22:09:18 -0700

    More header work and carousel draft

--- js/creative.js
+++ js/creative.js
@@ -45,4 +45,42 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
+    // Slick Slider init
+    $(document).ready(function(){
+
+        $('.screenshots').slick({
+            dots: true,
+            arrows: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    });
+
 })(jQuery); // End of use strict
